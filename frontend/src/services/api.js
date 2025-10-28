af42e2aa597c4cac6bf6f9aea1853e9b5c716aea
aman220 <122358408+priyanshigupta1706@users.noreply.github.com>
2025-10-28 13:10:50 +0530

    api update

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = process.env.REACT_APP_API_URL || 'https://moengage-assignment-2qql.onrender.com/api';
+const API_URL = 'https://moengage-assignment-2qql.onrender.com/api';
 
 const api = axios.create({
   baseURL: API_URL,
